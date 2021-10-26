bd08c00960109164522fe270daa8fe8bb163b4de
mi2-jarvis <miles2.jarvis@live.uwe.ac.uk>
2021-10-26 17:53:48 +0100

    Create basic pie chart rendering system

---
+++ KojosAvailability/KojosAvailability/KojosAvailability/Controllers/Graphs/PieChart/PieChartDataSet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KojosAvailability.Controllers.Graphs.PieChart
+{
+    public sealed class PieChartDataSet : DataSet
+    {
+
+    }
+}
